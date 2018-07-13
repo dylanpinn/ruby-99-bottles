7117545352d96e9f11cb1347a79032f74126b53c
Dylan Pinn <dylan@dylanpinn.com>
2018-07-13 13:40:19 +1000

    Make Factory open for change

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -21,14 +21,9 @@ end
 # Models a number of bottles.
 class BottleNumber
   def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
+    begin
+      const_get("BottleNumber#{number}")
+    rescue NameError
       BottleNumber
     end.new(number)
   end
