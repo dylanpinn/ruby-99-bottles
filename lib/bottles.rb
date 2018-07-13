26c310876e98f519c11847474925ced5596bc6f8
Dylan Pinn <dylan@dylanpinn.com>
2018-07-13 13:33:27 +1000

    Make requested change to 1 'six-pack'
    - Change 6 bottles -> 1 six-pack

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -26,6 +26,8 @@ class BottleNumber
       BottleNumber0
     when 1
       BottleNumber1
+    when 6
+      BottleNumber6
     else
       BottleNumber
     end.new(number)
@@ -85,5 +87,16 @@ class BottleNumber1 < BottleNumber
 
   def pronoun
     'it'
+  end
+end
+
+# Hanles when there is 1 six-pack left.
+class BottleNumber6 < BottleNumber
+  def quantity
+    '1'
+  end
+
+  def container
+    'six-pack'
   end
 end
